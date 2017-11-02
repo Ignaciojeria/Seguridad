30d183ab012b48d6282da94675ccaee7e97760ac
ignacio <Ignaciovl.j@gmail.com>
2017-11-02 02:48:06 -0300

    Agregando patron api gateway

--- zuul/src/main/java/com/example/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/example/zuul/ZuulApplication.java
@@ -1,12 +1,14 @@
-package com.example.seguridad;
+package com.example.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-public class SeguridadApplication {
+@EnableZuulProxy
+public class ZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeguridadApplication.class, args);
+		SpringApplication.run(ZuulApplication.class, args);
 	}
 }
--- zuul/src/test/java/com/example/zuul/ZuulApplicationTests.java
+++ zuul/src/test/java/com/example/zuul/ZuulApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.seguridad;
+package com.example.zuul;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SeguridadApplicationTests {
+public class ZuulApplicationTests {
 
 	@Test
 	public void contextLoads() {
